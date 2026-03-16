226f975e276c56406c9ef2ce310cc0261de64947
julianfigue26 <julian.figue26@gmail.com>
2026-03-16 11:47:40 -0500

    Update dashboard_maie.py

--- dashboard_maie.py
+++ dashboard_maie.py
@@ -1,9 +1,21 @@
-
+import streamlit as st
+import pandas as pd
+#
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 import requests
 from io import BytesIO
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 
 st.set_page_config(
     page_title="MAIE CU Pasto",
